602891891b0423f25d6ffee3eefd99bf9542bbbd
Bruno Antônio Joanucci <bruno.joanucci@aluno.imepac.edu.br>
2024-04-17 22:07:45 -0300

    adicionado MedicoService

--- src/main/java/br/edu/imepac/servicos/MedicoService.java
+++ src/main/java/br/edu/imepac/servicos/MedicoService.java
@@ -1,11 +1,23 @@
 package br.edu.imepac.servicos;
 
+import br.edu.imepac.DAOs.MedicoDAO;
 import br.edu.imepac.entidades.MedicoEntidade;
 
+import java.util.ArrayList;
+
 public class MedicoService {
-    MedicoDAO medicoDAO;
-    public int salvarMedico(MedicoEntidade medico){
-        return medicoDAO.save(medico);
+    private MedicoDAO medicoDAO;
+    public Integer salvarMedico(MedicoEntidade medicoEntidade){
+        return medicoDAO.save(medicoEntidade);
+    }
+    public ArrayList<MedicoEntidade> listarTodosMedicos(){
+        return medicoDAO.readAll();
+    }
+    public Integer atualizarMedico(MedicoEntidade medicoEntidade){
+        return medicoDAO.update(medicoEntidade);
+    }
+    public Integer deletarMedico(Long id){
+        return medicoDAO.delete(id);
     }
     
 }
